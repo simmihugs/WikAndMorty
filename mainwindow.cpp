0a504df10b98c733d82eafd8961cf4d7005387d0
simmihugs <simon.graetz@gmx.de>
2025-11-26 17:40:07 +0100

    Added simple calls to api

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,52 +1,71 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <QDebug>
-#include <QPushButton>
-#include <QList>
 
 MainWindow::MainWindow(QWidget *parent)
-  : QMainWindow(parent)
-  , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+
+  manager = new QNetworkAccessManager(this);
+  this->connect(manager, &QNetworkAccessManager::finished, this,
+                [=](QNetworkReply *reply) {
+                  if (reply->error()) {
+                    qDebug() << "Error:" << reply->errorString();
+                    return;
+                  }
+                  QString response = reply->readAll();
+                  handleJsonResponse(response);
+                });
+
   ui->setupUi(this);
 
-  QList<QPushButton*> myButtons = {
-    ui->pushButton_1,
-    ui->pushButton_2,
-    ui->pushButton_3,
-    ui->pushButton_4,
-    ui->pushButton_5,
-    ui->pushButton_6,
-    ui->pushButton_7,
-    ui->pushButton_8,
-    ui->pushButton_9
-  };
+  QList<QPushButton *> myButtons = {
+      ui->pushButton_1, ui->pushButton_2, ui->pushButton_3,
+      ui->pushButton_4, ui->pushButton_5, ui->pushButton_6,
+      ui->pushButton_7, ui->pushButton_8, ui->pushButton_9};
 
   int index = 1;
   for (QPushButton *button : myButtons) {
-      connect(button, &QPushButton::clicked,
-	  this,
-	      [this, index] {
-		this->handleGridButtonClick(index);
-	      }
-	      );
-      index++;
+    if (index == 3) {
+      this->connect(button, &QPushButton::clicked, this,
+                    [this, index] { this->handleGridButtonClick(index); });
+
+    } else {
+      this->connect(button, &QPushButton::clicked, this,
+                    &MainWindow::sendRequest);
+    }
+    index++;
+  }
+}
+
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::handleJsonResponse(const QString &jsonStr) {
+  QJsonParseError parseError;
+  QJsonDocument jsonDoc =
+      QJsonDocument::fromJson(jsonStr.toUtf8(), &parseError);
+
+  if (parseError.error != QJsonParseError::NoError) {
+    qDebug() << "JSON parse error:" << parseError.errorString();
+    qDebug() << "Raw response:" << jsonStr;
+    return;
   }
 
+  // Convert JSON document to nicely formatted string
+  QString prettyJson = jsonDoc.toJson(QJsonDocument::Indented);
+  qDebug().noquote() << "Formatted JSON:\n" << prettyJson;
 }
 
-void MainWindow::handleGridButtonClick(int index)
-{
-    // The 'index' parameter is now the button's ID (1-9)
-    qDebug() << "Button for Character ID" << index << "was clicked.";
-    
-    // Example usage: You can now easily construct the API URL
-    QString apiUrl = QString("https://rickandmortyapi.com/api/character/%1").arg(index);
-    qDebug() << "Fetching URL:" << apiUrl;
+void MainWindow::sendRequest() {
+  request.setUrl(QUrl("https://rickandmortyapi.com/api"));
+  manager->get(request);
 }
 
-MainWindow::~MainWindow()
-{
-  delete ui;
-}
+void MainWindow::handleGridButtonClick(int index) {
+  // The 'index' parameter is now the button's ID (1-9)
+  qDebug() << "Button for Character ID" << index << "was clicked.";
 
+  // Example usage: You can now easily construct the API URL
+  QString apiUrl =
+      // QString("https://rickandmortyapi.com/api/character/%1").arg(index);
+      QString("https://rickandmortyapi.com/api");
+  qDebug() << "Fetching URL:" << apiUrl;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,16 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QDebug>
+#include <QJsonDocument>
+#include <QJsonParseError>
+#include <QList>
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QPushButton>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,18 +18,21 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
+  QNetworkAccessManager *manager;
+  QNetworkRequest request;
 
 private slots:
   void handleGridButtonClick(int index);
+  void sendRequest();
+  void handleJsonResponse(const QString &jsonStr);
 };
 #endif // MAINWINDOW_H
